84dfc3545b8b1360323d36fdc4e91f62d194a610
전윤민 <cordelia273@naver.com>
2023-11-10 19:51:09 +0900

    config for webpack

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.module.rules.push({
+      test: /supabase/i,
+      loader: "ignore-loader",
+    });
+    return config;
+  },
 };
 
 module.exports = nextConfig;
